795977ab9b4b6e6c0916a4e3f629dfd234af95d4
YahyaMezzi <mezz00216@gmail.com>
2020-05-05 13:43:36 +0100

    updated SetupRelations

--- src/RelationManager/SetupRelations.java
+++ src/RelationManager/SetupRelations.java
@@ -1,6 +1,5 @@
 package RelationManager;
 
-
 import Grid.GridNode;
 
 import java.util.List;
@@ -9,16 +8,16 @@ import java.util.ListIterator;
 public class SetupRelations {
 
     public SetupRelations(List<GridNode> gridNodeList) {
-        ListIterator<GridNode> listItr = gridNodeList.listIterator();
-        ListIterator<GridNode> Itr = gridNodeList.listIterator();
-        while (listItr.hasNext()) {
-            GridNode hmm = listItr.next();
-            if (!(hmm.isObstacle())) {
-                while (Itr.hasNext()) {
-                    GridNode yikes = Itr.next();
-                    if (!(yikes.isObstacle())) {
-                        if (IsNeighbor(hmm, yikes))
-                            new Relation(hmm, yikes);
+
+
+        for(ListIterator<GridNode> listItr = gridNodeList.listIterator(); listItr.hasNext();) {
+            GridNode node1=listItr.next();
+            if (!node1.isObstacle()) {
+                for(ListIterator<GridNode> Itr = gridNodeList.listIterator();Itr.hasNext();) {
+                    GridNode node2=Itr.next();
+                    if (!(node2.isObstacle())) {
+                        if (IsNeighbor(node1, node2))
+                            new Relation(node1, node2);
                     }
                 }
             }
@@ -27,14 +26,19 @@ public class SetupRelations {
     }
 
     public boolean IsNeighbor(GridNode s, GridNode e) {
-        if ((s.getX() == e.getX()) && (s.getY() == e.getY() + 1))
-            return true;
-        if ((s.getX() == e.getX()) && (s.getY() == e.getY() - 1))
-            return true;
-        if ((s.getX() == e.getX() + 1) && (s.getY() == e.getY()))
-            return true;
-        if ((s.getX() == e.getX() - 1) && (s.getY() == e.getY()))
-            return true;
-        return false;
+        boolean b = false;
+        if (((int)s.getX() == (int) e.getX()) && ((int)s.getY() == (int)e.getY() + 1))
+        {b=true; }
+
+        if (((int)s.getX() ==(int) e.getX()) && ((int)s.getY() == (int)e.getY() - 1))
+        { b=true;}
+
+        if (((int)s.getX() == (int)e.getX() + 1) && ((int)s.getY() == e.getY()))
+        {b=true;}
+
+        if (((int)s.getX() == (int)e.getX() - 1) && ((int)s.getY() == (int)e.getY()))
+        {b=true;}
+        return b;
+
     }
 }
