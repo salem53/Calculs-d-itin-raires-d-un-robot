8ed71ffba62175a8b59e14852eae6a15324b77aa
IceBreaker8 <59843500+IceBreaker8@users.noreply.github.com>
2020-05-05 21:50:35 +0100

    Add files via upload

---
+++ src/utils/ProjectColors.java
@@ -0,0 +1,16 @@
+package utils;
+
+import javafx.scene.paint.Color;
+
+public class ProjectColors {
+
+
+    public static Color startColor = Color.GREEN;
+
+    public static Color endColor = Color.RED;
+
+    public static Color obstacleColor = Color.BLUE;
+
+    public static Color pathColor = Color.GREENYELLOW;
+
+}
